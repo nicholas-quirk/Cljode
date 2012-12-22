1e709c9ab9db9dfcba723c92f9d9e8e9d74ddaad
unknown <Nicholas@NICHOLAS-PC.(none)>
2012-12-22 12:22:20 -0800

    Removed the hard coded port 8080. Better let the user decide.

--- src/cljode/core.clj
+++ src/cljode/core.clj
@@ -12,5 +12,4 @@
 
 (defn -main [port]
   (models/initialize)
-  (let [port (Integer/parseInt (get (System/getenv) "PORT" "8080"))]
-  (run-jetty app {:port (Integer. port)})))
+  (run-jetty app {:port (Integer. port)}))
